99d68a6b5da0fabc694214476d3f7f8aa8fdc274
joshdelmonte <joshdelmonte@gmail.com>
2022-11-13 01:41:53 -0700

    Completed models and contents

---
+++ Develop/models/ProductTag.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+  //   * `id`
+
+  //   * Integer.
+
+  //   * Doesn't allow null values.
+
+  //   * Set as primary key.
+
+  //   * Uses auto increment.
+  id:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+
+  // * `product_id`
+
+  //   * Integer.
+
+  //   * References the `Product` model's `id`.
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: `Product`,
+      key: `id`
+    }
+  },
+
+  // * `tag_id`
+
+  //   * Integer.
+
+  //   * References the `Tag` model's `id`.
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references:{
+      model: `Tag`,
+      key: `id`
+    }
+  }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
