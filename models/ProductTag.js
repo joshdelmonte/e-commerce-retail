86d0cc3d3bd55825075044ee8c14526dcf18613b
joshdelmonte <joshdelmonte@gmail.com>
2023-02-19 15:48:15 -0700

    Finalizing and testing

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,42 +2,42 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
-  //   * `id`
-  //   * Integer.
-  //  * Doesn't allow null values.
-  //   * Set as primary key.
-  //   * Uses auto increment.
-  id:{
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  // * `product_id`
-  //   * Integer.
-  //   * References the `Product` model's `id`.
-  product_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: `product`,
-      key: `id`
+    //   * `id`
+    //   * Integer.
+    //  * Doesn't allow null values.
+    //   * Set as primary key.
+    //   * Uses auto increment.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // * `product_id`
+    //   * Integer.
+    //   * References the `Product` model's `id`.
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: `product`,
+        key: `id`
+      }
+    },
+    // * `tag_id`
+    //   * Integer.
+    //   * References the `Tag` model's `id`.
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: `tag`,
+        key: `id`
+      }
     }
-  },
-  // * `tag_id`
-  //   * Integer.
-  //   * References the `Tag` model's `id`.
-  tag_id: {
-    type: DataTypes.INTEGER,
-    references:{
-      model: `tag`,
-      key: `id`
-    }
-  }
   },
   {
     sequelize,
